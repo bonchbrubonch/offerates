5e3b10ff38e0314c9b1d80dfbaed5fbcd470871e
Artem <nikolloteslla@gmail.com>
2023-07-17 15:36:42 +0300

    add registration page s page`s bg

--- app/js/app.js
+++ app/js/app.js
@@ -21,24 +21,25 @@ $(function () {
 
   });
 
-  $(".type").select2({
+  $(".form-select").select2({
+    placeholder: "Выберите из списка ниже",
+    minimumResultsForSearch: -1
+  });
 
+  $(".type").select2({
     placeholder: "Категория оффера",
   });
 
   $(".search").select2({
     placeholder: "Поиск по офферу",
   });
-  
 
 
   $('.offers__bottom-like').on('click', function () {
     $(this).toggleClass('active');
   });
 
-
   $(function () {
- 
     $(".rateYo").rateYo({
       starWidth: "20px",
       ratedFill: "#F0B020",
@@ -46,21 +47,27 @@ $(function () {
       rating: 5,
       readOnly: true,
       onChange: function (rating, rateYoInstance) {
-   
         $(this).next().text(rating);
       }
-
-      
     });
   });
 
-    $("#rateYo-2").rateYo({
-      starWidth: "20px",
-      ratedFill: "#F0B020",
-      spacing: "2px",
-      rating: 5,
-   
-    });
-  
+  $("#rateYo-2").rateYo({
+    starWidth: "20px",
+    ratedFill: "#F0B020",
+    spacing: "2px",
+    rating: 5,
+
+  });
+
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 700,
+    arrows: false,
+    dots: true,
+  });
+
 
 });
